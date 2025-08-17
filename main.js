a783664c648303e26624bc7f583f1c54518b2142
MohsinAbdulSattar <mohsinabdulsattar7@gmail.com>
2025-08-17 21:14:10 +0500

    uploading js and logo

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+        $('#logo-wrapper').hover(
+            function () {
+                $('#logof').fadeIn(600);
+            },
+            function () {
+                $('#logof').fadeOut(600);
+            }
+        );
+
+            $('#leady').hide();
+            $('#leady').show(800);
+            $('#slide').hide();
+            $('#slide').show(900);
+            $('#slide2').hide();
+            $('#slide2').show(1800);
+            $('#intro').hide();
+            $('#intro').show(2000);
+            $('#desc').hide();
+            $('#desc').show(1000);
+
+              });
